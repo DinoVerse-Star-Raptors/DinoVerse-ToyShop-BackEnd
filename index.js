ed84fb558c7bd7f32ef2457cde0fdb2987fa13c8
Virachai Wongsena <wvirachai@gmail.com>
2024-11-25 17:04:44 +0700

    disable credentials

--- index.js
+++ index.js
@@ -70,8 +70,8 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  allowedHeaders: ['Content-Type', 'Authorization']
+  //   credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 
 // Apply CORS middleware with options
